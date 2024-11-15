b2c61b65a52974dd1b48ad2dd37e1816923f0057
mooleshacat <admin@catspeed.cc>
2024-11-15 17:48:35 -0500

    Update scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,15 +9,6 @@ cd /scripts/
 echo "# start of logfile" > entrypoint.log
 echo "# start of logfile" > generate-tokens.log
 
-# install NVM & Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-npm install v22.11.0
-npm use v22.11.0
-
 # git token generator
 git clone https://github.com/YunzheZJU/youtube-po-token-generator.git
 
