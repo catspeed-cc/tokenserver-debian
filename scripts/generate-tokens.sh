95678dff031de18df33b24e08043bb710001ad11
mooleshacat <admin@catspeed.cc>
2024-11-15 17:25:36 -0500

    Update scripts/generate-tokens.sh

--- scripts/generate-tokens.sh
+++ scripts/generate-tokens.sh
@@ -4,7 +4,7 @@
 
 echo "generate-tokens.sh - for generating tokens - due to technical reasons, only works with catspeed fork!"
 
-echo "$1" >> /scripts/generate-tokens.log
+echo "${1}" >> /scripts/generate-tokens.log
 
 # EOF
 
