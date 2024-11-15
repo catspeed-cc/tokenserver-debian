ea074a462883857aa9038fe9a36c082b27980c0c
mooleshacat <admin@catspeed.cc>
2024-11-15 17:07:26 -0500

    Add scripts/entrypoint.sh

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!
+
+echo "entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!"
