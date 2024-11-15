75bf9a79209d076da01c1b9fad30cd69f59a1946
mooleshacat <admin@catspeed.cc>
2024-11-15 17:15:30 -0500

    Update scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,8 @@
 
 echo "entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!"
 
-
+read -a instances <<< "${INSTANCES}"
+echo "${array[0]} ${array[1]}"
 
 # EOF
 
