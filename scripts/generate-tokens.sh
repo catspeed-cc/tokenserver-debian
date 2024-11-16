8bfd1de84d336fd44d78f5721e4854d73573d6bb
mooleshacat <admin@catspeed.cc>
2024-11-15 21:02:40 -0500

    Update scripts/generate-tokens.sh

--- scripts/generate-tokens.sh
+++ scripts/generate-tokens.sh
@@ -6,19 +6,18 @@ echo "" | tee -a /scripts/generate-tokens.log
 echo "generate-tokens.sh - for generating tokens - due to technical reasons, only works with catspeed fork!" | tee -a /scripts/generate-tokens.log
 echo "" | tee -a /scripts/generate-tokens.log
 
-echo "generating ${NUM_TOKENS} tokens for ${1}" | tee -a /scripts/generate-tokens.log
+echo "[${1}] generating ${NUM_TOKENS} tokens for ${1}" | tee -a /scripts/generate-tokens.log
 
 # infinite loop because
 while true; 
 do
     # loop through each number
-    echo "random start number ${rnd}" | tee -a /scripts/generate-tokens.log
     for (( c=0; c<=$NUM_TOKENS; c++ ))
     do
 
         # decide what our key is
         the_key=invidious:ANON-${1}-${c}
-        echo "key name: ${the_key}" | tee -a /scripts/generate-tokens.log
+        echo "[${1}] key name: ${the_key}" | tee -a /scripts/generate-tokens.log
 
         # check if tokens exist in redis
         po_token=$(redis-cli -h ${REDIS_HOST} -p ${REDIS_PORT} GET ${the_key}:po_token)
@@ -27,20 +26,35 @@ do
         # if no tokens exist, generate them
         if [[ -z "${po_token}" ]] || [[ -z "${visitor_data}" ]]; then
             
-            echo "token(s) are empty, generating ..." | tee -a /scripts/generate-tokens.log
+            echo "[${1}] token(s) are empty, generating ..." | tee -a /scripts/generate-tokens.log
 
             # generate tokens :D
 
             token_data=$(/usr/bin/node /scripts/youtube-po-token-generator/examples/one-shot.js)
-            echo "token Data: ${token_data}" | tee -a /scripts/generate-tokens.log
+            
+            # sanity check if length > 0
+            if [ -n "$token_data" ]; then
+                
+                # extract tokens
+
+                # sanity check if length > 0
+
+                # store tokens in redis
+
+            else
+
+                # token_data is empty
+                echo "[${1}] token_data: ${token_data}" | tee -a /scripts/generate-tokens.log
+
+            fi
 
             # store the tokens in redis
         
         else
 
-            echo "token(s) exist ..." | tee -a /scripts/generate-tokens.log
-            echo "po_token: '${po_token}'" | tee -a /scripts/generate-tokens.log
-            echo "visitor_data: '${visitor_data}'" | tee -a /scripts/generate-tokens.log
+            echo "[${1}] token(s) exist ..." | tee -a /scripts/generate-tokens.log
+            echo "[${1}] po_token: '${po_token}'" | tee -a /scripts/generate-tokens.log
+            echo "[${1}] visitor_data: '${visitor_data}'" | tee -a /scripts/generate-tokens.log
 
         fi
 
