f9b483afd32afdc689e9754bdc5307f84879f8ba
mooleshacat <admin@catspeed.cc>
2024-11-15 20:04:09 -0500

    Update scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,37 +2,34 @@
 
 # entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!
 
-# change to scripts directory
-cd /scripts/
-
 # clear out logs
-echo "# start of logfile" | tee entrypoint.log
-echo "# start of logfile" | tee generate-tokens.log
+echo "# start of logfile" | tee /scripts/entrypoint.log
+echo "# start of logfile" | tee /scripts/generate-tokens.log
 
-echo "" | tee -a entrypoint.log
-echo "entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!" | tee -a entrypoint.log
-echo "" | tee -a entrypoint.log
+echo "" | tee -a /scripts/entrypoint.log
+echo "entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!" | tee -a /scripts/entrypoint.log
+echo "" | tee -a /scripts/entrypoint.log
 
 # change to token generator directory
 cd /scripts/youtube-po-token-generator/
 
 # testrun the script
-node examples/one-shot.js | tee -a entrypoint.log
+node examples/one-shot.js | tee -a /scripts/entrypoint.log
 
-# change back to scripts directory
+# change to scripts directory
 cd /scripts/
 
-echo "executing generator scripts" | tee -a  entrypoint.log
+echo "executing generator scripts" | tee -a /scripts/entrypoint.log
 
 # execute script for each instance
 IFS='|' read -a theinstances <<< "${INSTANCES}"
 for instance in "${theinstances[@]}"
 do
-   echo "$instance" >> entrypoint.log
+   echo "$instance" | tee -a /scripts/entrypoint.log
    /scripts/generate-tokens.sh $instance &
 done
 
-echo "done executing generator scripts" | tee -a  entrypoint.log
+echo "done executing generator scripts" | tee -a /scripts/entrypoint.log
 
 # this 'hack' will keep container awake and running
 # may remove at future date/time
