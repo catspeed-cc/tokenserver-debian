1b8ce97c2b4dd7028277e3b45fd5801e11d37d6b
mooleshacat <admin@catspeed.cc>
2024-11-15 20:01:58 -0500

    Update scripts/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,25 +2,27 @@
 
 # entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!
 
-echo ""
-echo "entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!"
-echo ""
-
 # change to scripts directory
 cd /scripts/
 
 # clear out logs
-echo "# start of logfile" > entrypoint.log
-echo "# start of logfile" > generate-tokens.log
+echo "# start of logfile" | tee entrypoint.log
+echo "# start of logfile" | tee generate-tokens.log
+
+echo "" | tee -a entrypoint.log
+echo "entrypoint.sh - for generating tokens - due to technical reasons, only works with catspeed fork!" | tee -a entrypoint.log
+echo "" | tee -a entrypoint.log
 
 # change to token generator directory
 cd /scripts/youtube-po-token-generator/
 
 # testrun the script
-node examples/one-shot.js >> entrypoint.log
+node examples/one-shot.js | tee -a entrypoint.log
 
 # change back to scripts directory
 cd /scripts/
+
+echo "executing generator scripts" | tee -a  entrypoint.log
 
 # execute script for each instance
 IFS='|' read -a theinstances <<< "${INSTANCES}"
@@ -29,6 +31,8 @@ do
    echo "$instance" >> entrypoint.log
    /scripts/generate-tokens.sh $instance &
 done
+
+echo "done executing generator scripts" | tee -a  entrypoint.log
 
 # this 'hack' will keep container awake and running
 # may remove at future date/time
