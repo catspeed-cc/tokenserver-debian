415aaa9714f4cfef896099adc7f96e5443059d48
mooleshacat <admin@catspeed.cc>
2024-11-16 04:27:22 -0500

    Update scripts/generate-user-tokens.sh

--- scripts/generate-user-tokens.sh
+++ scripts/generate-user-tokens.sh
@@ -42,38 +42,7 @@ do
             the_key=invidious:USER-${the_user}-${$instance}
             echo "[${the_key}] Generating tokens" | tee -a /scripts/generate-user-tokens.log
 
-            token_data=$(/usr/bin/node /scripts/youtube-po-token-generator/examples/one-shot.js)
-            
-            # sanity check if length > 0
-            if [[ -n "$token_data" ]]; then
-                
-                # extract tokens
-                po_token=$(echo ${token_data} | awk -F"'" '/poToken/{print $4}')
-                visitor_data=$(echo ${token_data} | awk -F"'" '/visitorData/{print $2}')
 
-                # sanity check if length > 0
-                if [[ -n "$po_token" ]] && [[ -n "$visitor_data" ]]; then
-
-                    # store tokens in redis
-                    redis-cli -h ${REDIS_HOST} -p ${REDIS_PORT} SET ${the_key}:po_token ${po_token} EX ${USER_EXPIRY}
-                    redis-cli -h ${REDIS_HOST} -p ${REDIS_PORT} SET ${the_key}:visitor_data ${visitor_data} EX ${USER_EXPIRY}
-                    echo "[${the_key}] STORED IN REDIS: po_token: '${po_token}'" | tee -a /scripts/generate-user-tokens.log
-                    echo "[${the_key}] STORED IN REDIS: visitor_data: '${visitor_data}'" | tee -a /scripts/generate-user-tokens.log
-
-                else
-
-                    # tokens empty
-                    echo "[${the_key}] EMPTY TOKENS? po_token: '${po_token}'" | tee -a /scripts/generate-user-tokens.log
-                    echo "[${the_key}] EMPTY TOKENS? visitor_data: '${visitor_data}'" | tee -a /scripts/generate-user-tokens.log
-
-                fi
-
-            else
-
-                # token_data is empty
-                echo "[${the_key}] token_data: ${token_data}" | tee -a /scripts/generate-user-tokens.log
-
-            fi
 
         done
 
