f06860db235e8c4f1aa671dd6778350846f4eaf0
mooleshacat <admin@catspeed.cc>
2024-11-16 21:10:49 -0500

    Update scripts/generate-tokens.sh

--- scripts/generate-tokens.sh
+++ scripts/generate-tokens.sh
@@ -31,6 +31,7 @@ do
             # generate tokens :D
 
             token_data=$(/usr/bin/node /scripts/youtube-po-token-generator/examples/one-shot.js)
+            echo "[${1}] token_data: '${token_data}'" | tee -a /scripts/generate-tokens.log
             
             # sanity check if length > 0
             if [[ -n "$token_data" ]]; then
