b8e58aa48a979d56114a40e5e450e86ac96dfa55
mooleshacat <admin@catspeed.cc>
2024-11-17 20:55:24 -0500

    Update scripts/generate-tokens.sh

--- scripts/generate-tokens.sh
+++ scripts/generate-tokens.sh
@@ -30,16 +30,20 @@ do
 
             # generate tokens :D
 
-            node youtube-po-token-generator/examples/one-shot.js | tee -a /scripts/generate-tokens.log
+            #node youtube-po-token-generator/examples/one-shot.js | tee -a /scripts/generate-tokens.log
 
             # test
             #test=$(which which)
             #echo "[${1}] test: '${test}'" | tee -a /scripts/generate-tokens.log
 
-            #token_data=$(/usr/bin/node youtube-po-token-generator/examples/one-shot.js)
-            #echo "[${1}] token_data: '${token_data}'" | tee -a /scripts/generate-tokens.log
+            echo "PID: ${!}"
 
-            wait
+            token_data=$(/usr/bin/node youtube-po-token-generator/examples/one-shot.js)
+            echo "[${1}] token_data: '${token_data}'" | tee -a /scripts/generate-tokens.log
+
+            echo "PID: ${!}"
+
+            wait -n
             
             # sanity check if length > 0
             if [[ -n "$token_data" ]]; then
