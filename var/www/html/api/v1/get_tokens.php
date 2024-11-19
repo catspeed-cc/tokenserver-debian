c86aff3cc6d571008e01475ad0809bdb097276ec
mooleshacat <admin@catspeed.cc>
2024-11-19 12:29:25 -0500

    fixed comments
    forgot php uses // for comment not #

--- var/www/html/api/v1/get_tokens.php
+++ var/www/html/api/v1/get_tokens.php
@@ -1,20 +1,30 @@
 <?php
 
+// set the content-type header to text/json
 header("Content-Type: text/json");
 
+// grab some env vars
 $server_id = getenv('SERVER_ID');
 $num_tokens = getenv('NUM_TOKENS');
 $token_expiry = getenv('TOKEN_EXPIRY');
 
+// initialize redis connection
 $redis = new Redis();
+
+// hardcoded to localhost because we only use local redis db
+// each tokenserver has it's own redis db
 $redis->connect('localhost', 6379);
 
+// works to test the condition in the following while loop
 $token_data = "";
 
+// while redis data is empty
 while(strlen($token_data) <= 25) {
 
+    // pick a random number between 0 and NUM_TOKENS (from env)
     $rnd = rand(0,$num_tokens);
 
+    // generate the keyname
     $the_key = "tokenserver:TOKEN-$rnd:tokendata";
 
     // Statements to be executed
@@ -22,7 +32,7 @@ while(strlen($token_data) <= 25) {
 
 }
 
-# todo: delete redis key, so all keys are unique - may as well :3c
+// todo: delete redis key, so all keys are unique - may as well :3c
 
 $token_data_array = explode("\n", $token_data);
 
@@ -35,9 +45,9 @@ $token_data = implode("\n", $token_data_array);
 $token_data = str_replace("poToken", "po_token", $token_data);
 $token_data = str_replace("visitorData", "visitor_data", $token_data);
 
-# todo: swap element 2 & 3 because it bothers me
+// todo: swap element 2 & 3 because it bothers me
 
-# looking at page, it looks like no new lines, but looking at source, newlines are there.
+// looking at page, it looks like no new lines, but looking at source, newlines are there.
 echo $token_data;
 
 ?>
