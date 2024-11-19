79e638007f8796d70730b64957484ef11316e896
mooleshacat <admin@catspeed.cc>
2024-11-19 18:16:05 -0500

    Update var/www/html/api/v1/get_tokens.php

--- var/www/html/api/v1/get_tokens.php
+++ var/www/html/api/v1/get_tokens.php
@@ -56,7 +56,8 @@ $token_data_array[2] = $element3;
 $token_data_array[3] = $element2;
 
 // append error (OK) to end of array
-array_splice($token_data_array, (max($token_data_array)-1), 0, "  error: 'OK'");
+$theindex=max($token_data_array)-1
+array_splice($token_data_array, $theindex, 0, "  error: 'OK'");
 
 // implode array back into string using \n
 $token_data = implode("\n", $token_data_array);
