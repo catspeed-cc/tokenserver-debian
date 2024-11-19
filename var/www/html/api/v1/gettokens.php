072742c506a144fc01f8f304650e8cc909597a00
mooleshacat <admin@catspeed.cc>
2024-11-19 00:41:02 -0500

    Update var/www/html/api/v1/gettokens.php

--- var/www/html/api/v1/gettokens.php
+++ var/www/html/api/v1/gettokens.php
@@ -21,14 +21,6 @@ array_splice( $token_data_array, 1, 0, $token_data_add );
 
 $token_data = implode("\n", $token_data_array);
 
-print_r($token_data);
-
-echo "<br />";
-
-$token_data = str_replace("'", '"', $token_data);
-
-echo "<br />token_data:<br >";
-
 # nl2br will preserve \n - if causes problem use str_replace
 echo nl2br($token_data);
 
