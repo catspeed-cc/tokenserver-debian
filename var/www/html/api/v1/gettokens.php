f1b09ad065d24f99542583fb287013ad9fc580de
mooleshacat <admin@catspeed.cc>
2024-11-18 23:18:41 -0500

    Update var/www/html/api/v1/gettokens.php

--- var/www/html/api/v1/gettokens.php
+++ var/www/html/api/v1/gettokens.php
@@ -4,16 +4,14 @@ $server_id = getenv('SERVER_ID');
 $num_tokens = getenv('NUM_TOKENS');
 $token_expiry = getenv('TOKEN_EXPIRY');
 
-//echo "NUM_TOKENS: '$num_tokens'<br />";
-//echo "TOKEN_EXPIRY: '$token_expiry'<br />";
-
 $rnd = rand(0,$num_tokens);
 
 $the_key = "tokenserver:TOKEN-$rnd:tokendata";
 
 $redis = new Redis();
 $redis->connect('localhost', 6379);
-$token_data = $redis->keys('$the_key');
+
+$token_data = $redis->keys("$the_key");
 
 echo "token_data: $token_data";
 
