8a10c69bfcb2ff52650d242f1a1a7953cdf891e7
mooleshacat <admin@catspeed.cc>
2024-11-19 16:13:46 -0500

    Update var/www/html/api/v1/get_tokens.php

--- var/www/html/api/v1/get_tokens.php
+++ var/www/html/api/v1/get_tokens.php
@@ -28,7 +28,7 @@ while(strlen($token_data) <= 25) {
     $rnd = rand(0,$num_tokens);
 
     // generate the keyname
-    $the_key = "tokenserver:TOKEN-$rnd:tokendata";
+    $the_key = "tokenserver:TOKEN:tokendata-${rnd}";
 
     // get the tokens from redis
     $token_data = $redis->get($the_key);
