87a4b3d7f46e5be494fc6aa3b7bf0d11fa855b45
mooleshacat <admin@catspeed.cc>
2024-11-18 23:07:50 -0500

    Update var/www/html/api/v1/gettokens.php

--- var/www/html/api/v1/gettokens.php
+++ var/www/html/api/v1/gettokens.php
@@ -3,7 +3,9 @@
 $num_tokens = getenv('NUM_TOKENS');
 $token_expiry = getenv('TOKEN_EXPIRY');
 
-echo "NUM_TOKENS: '$num_tokens'<br />";
-echo "TOKEN_EXPIRY: '$token_expiry'<br />";
+//echo "NUM_TOKENS: '$num_tokens'<br />";
+//echo "TOKEN_EXPIRY: '$token_expiry'<br />";
+
+$rnd = rand(0,$num_tokens);
 
 ?>
