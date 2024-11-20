896ea0b37d47a3f1b275bf6002e460a9858e4cb8
mooleshacat <admin@catspeed.cc>
2024-11-20 13:09:40 -0500

    Update scripts/randomize_passwords.sh

--- scripts/randomize_passwords.sh
+++ scripts/randomize_passwords.sh
@@ -6,5 +6,5 @@ echo ""
 
 tmppass=$(tr -dc 'A-Za-z0-9!?@#$%^&*(){-}[+]<>|\/_=,.:;' </dev/urandom | head -c 13)
 
-echo "${tmppass}" | tee ./secrets/tokenserver-root-password
+echo "${tmppass}" | tee ./tokenserver-data/secrets/tokenserver-root-password
 echo ""
