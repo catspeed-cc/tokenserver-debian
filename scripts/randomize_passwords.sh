40cbc7769104e521f09fbda20b212504269e2346
mooleshacat <admin@catspeed.cc>
2024-11-20 11:06:02 -0500

    Update scripts/randomize_passwords.sh

--- scripts/randomize_passwords.sh
+++ scripts/randomize_passwords.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+echo ""
+echo "generating root password"
+echo ""
+
 tmppass=$(tr -dc 'A-Za-z0-9!?@#$%^&*(){-}[+]<>|\/_=,.:;' </dev/urandom | head -c 13)
 
 echo "${tmppass}" | tee ./secrets/tokenserver-root-password
+echo ""
